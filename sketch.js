04d6328a3e0baea24aee67239aeba2ac362e2d72
Indigo Lily <yulex.42@gmail.com>
2017-08-12 08:35:35 -0300

    Added visual output

--- sketch.js
+++ sketch.js
@@ -1,77 +1,58 @@
 Array.prototype.last = function () {
-    return this[this.length - 1];
+  return this[this.length - 1];
 }
 
 function card(rank = 2, suit = 0) {
-    // ace is 14;
-    // suits go from 0 to 3, in this order: diamonds, clubs, hearts, spades
-    this.rank = rank;
-    this.suit = suit;
+  // ace is 14;
+  // suits go from 0 to 3, in this order: spades, hearts, diamonds, clubs
+  this.rank = rank;
+  this.suit = suit;
 
-    this.toString = function () {
-        // convert to unicode card character and return that
+  this.toString = function () {
+    // convert to unicode card character and return that
+    var output = '0x1F0';
 
-        // temporary
-        var output = "";
-        switch (this.rank) {
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                output += this.rank;
-                break;
-            case 10:
-                output += "T";
-                break;
-            case 11:
-                output += "J";
-                break;
-            case 12:
-                output += "Q";
-                break;
-            case 13:
-                output += "K";
-                break;
-            case 14:
-                output += "A";
-                break;
-            default:
-                output += "?";
-                break;
-        }
-        switch (this.suit) {
-            case 0:
-                output += "♦";
-                break;
-            case 1:
-                output += "♣";
-                break;
-            case 2:
-                output += "♥";
-                break;
-            case 3:
-                output += "♠";
-                break;
-            default:
-                output += "?";
-                break;
-        }
-        return output;
+    switch (this.suit) {
+      case 0:
+        output += 'A';
+        break;
+      case 1:
+        output += 'B';
+        break;
+      case 2:
+        output += 'C';
+        break;
+      case 3:
+        output += 'D';
+        break;
+      default:
+        return '?';
     }
+
+    if (rank === 14) {
+      output += 1;
+    } else if (rank >= 12) {
+      output += (this.rank+1).toString(16);
+    } else {
+      output += this.rank.toString(16);
+    }
+
+    return String.fromCodePoint(output);
+  }
+  this.toHTML = function() {
+    var colour = 'card ' + ((this.suit === 1 || this.suit === 2) ? 'red' : 'black');
+    return `<span class='${colour}'>${this.toString()}</span>`;
+  }
 }
 
 function compare(card1, card2) {
-    if (card1.rank === card2.rank) {
-        return 0;
-    } else if (card1.rank > card2.rank) {
-        return 1;
-    } else {
-        return 2;
-    }
+  if (card1.rank === card2.rank) {
+    return 0;
+  } else if (card1.rank > card2.rank) {
+    return 1;
+  } else {
+    return 2;
+  }
 }
 
 var deck = [];
@@ -83,85 +64,97 @@ const player2 = document.getElementById('player2');
 const win = document.getElementById('win');
 const button = document.getElementById('reset');
 
+var speed = 1;
+
 function deal() {
-    var toggle = true;
-    while (deck.length) {
-        if (toggle) {
-            hand1.push(deck.splice((Math.floor(Math.random() * deck.length)), 1)[0]);
-        } else {
-            hand2.push(deck.splice((Math.floor(Math.random() * deck.length)), 1)[0]);
-        }
-        toggle = !toggle;
+  var toggle = true;
+  while (deck.length) {
+    if (toggle) {
+      hand1.push(deck.splice((Math.floor(Math.random() * deck.length)), 1)[0]);
+    } else {
+      hand2.push(deck.splice((Math.floor(Math.random() * deck.length)), 1)[0]);
     }
-    player1.innerText = hand1.length;
-    player2.innerText = hand2.length;
+    toggle = !toggle;
+  }
+  player1.innerText = hand1.length;
+  player2.innerText = hand2.length;
 
 }
 
 function reset() {
-    console.clear();
-    deck = [];
-    hand1 = [];
-    hand2 = [];
-    for (let suit = 0; suit <= 3; suit++) {
-        for (let rank = 2; rank <= 14; rank++) {
-            deck.push(new card(rank, suit));
-        }
+  console.clear();
+  deck = [];
+  hand1 = [];
+  hand2 = [];
+  for (let suit = 0; suit <= 3; suit++) {
+    for (let rank = 2; rank <= 14; rank++) {
+      deck.push(new card(rank, suit));
     }
-    win.innerText = '';
-    button.style.display = 'none';
-    deal();
+  }
+  win.innerText = '';
+  button.disabled = true;
+  document.getElementById('turns').innerHTML = '';
+  deal();
 }
 
-function h() {
-    return hand1.length > 0 && hand2.length > 0;
+function over() {
+  return hand1.length > 0 && hand2.length > 0;
 }
 
 function war( garbo = 0, level = 0) {
-    var pool = [hand1.pop(), hand2.pop()];
-    var winner = compare(...pool);
+  var pool = [hand1.pop(), hand2.pop()];
+  var winner = compare(...pool);
 
-    if (!winner) {
-        for (let i = 0; i < 3 && h(); i++) {
-            pool.push(hand1.pop());
-            pool.push(hand2.pop());
-        }
-        winner = h() ? war( 0, level + 1 ) : (hand1.length > 0) ? 1 : 2;
+  var turn = document.createElement('p');
+  turn.innerHTML = `${pool[0].toHTML()} ${['=','>','<'][winner]} ${pool[1].toHTML()}`;
+  var turns = document.getElementById('turns');
+  turns.insertBefore(turn, document.querySelector('#turns p'));
+
+  if (!winner) {
+    for (let i = 0; i < 3 && over(); i++) {
+      pool.push(hand1.pop());
+      pool.push(hand2.pop());
     }
+    winner = over() ? war( 0, level + 1 ) : (hand1.length > 0) ? 1 : 2;
+  }
 
-    while(pool.length) {
-        let pick = (Math.random() * pool.length) | 0;
-        let card = pool.splice( pick, 1 )[0]
+  turn.innerHTML += ` --> Player ${winner} gets: `;
+  pool.forEach(x => turn.innerHTML += x.toHTML());
 
-        if (winner === 1 || !hand2.length) {
-            hand1.unshift( card );
-        } else if (winner === 2 || !hand1.length) {
-            hand2.unshift( card );
-        }
+  while(pool.length) {
+    let pick = (Math.random() * pool.length) | 0;
+    let card = pool.splice( pick, 1 )[0]
+
+    if (winner === 1 || !hand2.length) {
+      hand1.unshift( card );
+    } else if (winner === 2 || !hand1.length) {
+      hand2.unshift( card );
     }
+  }
 
-    if (level) {
-        console.log(`Level ${level} war`)
-        return winner;
+  if (level) {
+    console.log(`Level ${level} war`)
+    return winner;
+  } else {
+    player1.innerText = hand1.length;
+    player2.innerText = hand2.length;
+    if ( over() ) {
+      // requestAnimationFrame(war);
+      setTimeout(war, 2000/speed);
     } else {
-        player1.innerText = hand1.length;
-        player2.innerText = hand2.length;
-        if ( h() ) {
-            requestAnimationFrame(war);
-        } else {
-            // game is over
-            var winstr = `Player ${hand1.length > 0 ? 1 : 2} wins!`
-            win.innerText = winstr;
-            button.style.display = 'block';
-            console.log(winstr);
-            return;
-        }
+      // game is over
+      var winstr = `Player ${hand1.length > 0 ? 1 : 2} wins!`
+      win.innerText = winstr;
+      button.disabled = false;
+      console.log(winstr);
+      return;
     }
+  }
 }
 
 button.onclick = function() {
-    reset();
-    war();
+  reset();
+  war();
 }
 
 reset();
